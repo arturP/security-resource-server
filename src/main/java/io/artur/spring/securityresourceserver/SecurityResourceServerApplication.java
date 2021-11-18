ec6a11dc319454a85af7ac4d048cf69048ebcd25
Artur Przychodzen <artur2p@gmail.com>
2021-11-18 17:42:34 +0100

    init

---
+++ src/main/java/io/artur/spring/securityresourceserver/SecurityResourceServerApplication.java
@@ -0,0 +1,13 @@
+package io.artur.spring.securityresourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityResourceServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/artur/spring/securityresourceserver/SecurityResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.artur.spring.securityresourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
